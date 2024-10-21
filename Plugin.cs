0551318ad5e7c78f2b3ca794947e268969a632dd
SergeyBezuglov <143338316+SergeyBezuglov@users.noreply.github.com>
2024-10-21 11:05:47 +0300

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,88 @@
+﻿using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+using PhoneApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace UserLoaderPlugin
+{
+    [PhoneApp.Domain.Attributes.Author(Name = "Ivan Petrov")]
+    public class Plugin : IPluggable
+    {
+        private static readonly NLog.Logger logger = LogManager.GetCurrentClassLogger();
+
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+        {
+            logger.Info("Запуск плагина загрузки пользователей");
+            var employeesList = args.Cast<EmployeesDTO>().ToList();
+
+            try
+            {
+                // Загрузка новых пользователей из API
+                var newUsers = LoadUsersFromApi().GetAwaiter().GetResult();
+
+                // Добавляем новых пользователей в список сотрудников
+                foreach (var user in newUsers)
+                {
+                    var employee = new EmployeesDTO
+                    {
+                        Name = $"{user.firstName} {user.lastName}"
+                    };
+                    employee.AddPhone(user.phone);
+                    employeesList.Add(employee);
+                    logger.Info($"Добавлен новый пользователь: {employee.Name}, Телефон: {user.phone}");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Ошибка при загрузке пользователей: {ex.Message}");
+                logger.Trace(ex.StackTrace);
+            }
+
+            return employeesList.Cast<DataTransferObject>();
+        }
+
+        private async Task<List<UserDTO>> LoadUsersFromApi()
+        {
+            string apiUrl = "https://dummyjson.com/users"; // URL для загрузки данных
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    var response = await httpClient.GetStringAsync(apiUrl);
+                    var result = JsonConvert.DeserializeObject<UserApiResponse>(response);
+                    return result.users;
+                }
+                catch (HttpRequestException httpEx)
+                {
+                    logger.Error($"Ошибка HTTP при получении данных: {httpEx.Message}");
+                    throw;
+                }
+                catch (Exception ex)
+                {
+                    logger.Error($"Ошибка при обработке данных из API: {ex.Message}");
+                    throw;
+                }
+            }
+        }
+    }
+
+    // DTO классы для работы с API
+    public class UserDTO
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phone { get; set; }
+    }
+
+    public class UserApiResponse
+    {
+        public List<UserDTO> users { get; set; }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("UserLoaderPlugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UserLoaderPlugin")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1e2955f0-e3a8-4592-ba9e-eb824ec6d296")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
